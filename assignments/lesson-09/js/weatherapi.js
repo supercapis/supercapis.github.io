e4cc2b680f05543d655afe6b4528c706597467ae
Capis Prestes <supercapis@gmail.com>
2018-11-14 21:43:59 -0700

    correcting names in nav

--- assignments/lesson-09/js/weatherapi.js
+++ assignments/lesson-09/js/weatherapi.js
@@ -1,34 +1,23 @@
-let weatherObject = new XMLHttpRequest();
-weatherObject.open = 'https://api.openweathermap.org/data/2.5/weather?id=2172797';
-weatherObject.send();
-weatherObject.onload = function () {
-    let weatherInfo = JSON.parse(weatherObject.responseText);
+let weatherSource = new XMLHttpRequest();
+let url = "https://api.openweathermap.org/data/2.5/weather?id=5604473&appid=928cdffb6f7b8cf5714b4c2f8047e814&units=imperial";
+
+weatherSource.open("GET", url, true);
+
+weatherSource.send();
+
+weatherSource.onload = function() {
+    let weatherInfo = JSON.parse(weatherSource.responseText);
     console.log(weatherInfo);
 
-    document.getElementById('city').innerHTML = weatherInfo.name;
-    let temperature = weatherInfo.main.temp;
-    let wind = weatherInfo.wind.speed;
-    let humidity = weatherInfo.main.humidity;
-    let windchill = (35.74 + (0.6215 * temperature) - (35.75 * Math.pow(wind, 0.16)) + (0.4275 * temperature * Math.pow(wind, 0.16)));
-    let heatIndexCalc = (-42.379 + (2.049 * temperature) + (10.1433 * humidity) + (-0.2248 * temperature * humidity) + (-0.006837 * temperature * temperature) + (-0.05482 * humidity * humidity) + (0.001229 * temperature * temperature * humidity) + (0.00085282 * temperature * humidity * humidity) + (-0.00000199 * temperature * temperature * humidity * humidity));
-    let tempHigh = weatherInfo.main.temp_max;
-    let tempLow = weatherInfo.main.temp_min;
-    document.getElementById('currentTemp').innerHTML = Math.round(parseFloat(temperature));
-    document.getElementById('description').innerHTML = weatherInfo.weather["0"].main;
-    let iconcode = weatherInfo.weather["0"].icon;
-    let icon_path = "http://openweathermap.org/img/w/" + iconcode + ".png";
-    document.getElementById('weather_icon').src = icon_path;
-    if (temperature > 80) {
-        document.getElementById('windChill').innerHTML = "Heat Index: " + Math.round(parseFloat(heatIndexCalc));
-    }
-    else if (temperature > windchill) {
-        document.getElementById('windChill').innerHTML = "Windchill: " + Math.round(parseFloat(windchill));
-    } else {
-        document.getElementById('windChill').innerHTML = "Feels like: " + Math.round(parseFloat(temperature));
-    }
+    document.getElementById("cityName").innerHTML = weatherInfo.name;
+    document.getElementById("currently").innerHTML = weatherInfo.weather["0"].description;
+    document.getElementById("cityTemp").innerHTML = weatherInfo.main.temp;
+    document.getElementById("cityHumid").innerHTML = weatherInfo.main.humidity;
+    document.getElementById("cityWindChill").innerHTML = weatherInfo.wind.speed;
 
-    document.getElementById('windSpeed').innerHTML = Math.round(parseFloat(wind));
-    document.getElementById('highTemp').innerHTML = Math.round(parseFloat(tempHigh));
-    document.getElementById('lowTemp').innerHTML = Math.round(parseFloat(tempLow));
+    let weatherIcon = weatherInfo.weather["0"].icon;
+    let iconLocation = "https://openweathermap.org/img/w/" + weatherIcon + ".png";
 
+    document.getElementById("current-conditions").src = iconLocation;
+    document.getElementById("cityMain").innerHTML = weatherInfo.weather["0"].main;
 }
