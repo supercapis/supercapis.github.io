2f52566830fa4769d87e33c65e0f564175db8a74
Capis Prestes <supercapis@gmail.com>
2018-11-21 08:17:16 -0700

    Deleting windchill from the folder

--- assignments/weathersite/js/windchill.js
+++
@@ -1,4 +0,0 @@
-let currenttemp = parseFloat(document.getElementById('currenttemp').innerHTML);
-let windspeed = parseFloat(document.getElementById('windspeed').innerHTML);
-var f= 35.74 + 0.6215 * currenttemp - 35.75 * Math.pow(windspeed,0.16) + 0.4275 * currenttemp *Math.pow(windspeed,0.16);
-document.getElementById("windchill").innerHTML= Math.round(f);
