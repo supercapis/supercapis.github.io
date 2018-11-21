ca275a71daddf97d699a51ba864697affeae21a8
Capis Prestes <supercapis@gmail.com>
2018-11-20 21:46:30 -0700

    Fixing the temperatures

---
+++ assignments/weathersite/js/forecastfishhaven.js
@@ -0,0 +1,56 @@
+//This file pushes api to the 5 days forecast table//
+
+let fRequestURL = 'https://api.openweathermap.org/data/2.5/forecast?id=5603240&appid=928cdffb6f7b8cf5714b4c2f8047e814&units=imperial';
+let fRequest = new XMLHttpRequest();
+fRequest.open('GET', fRequestURL);
+fRequest.responseType = 'json';
+fRequest.send();
+fRequest.onload = function () {
+    let fData = fRequest.response;
+    console.log(fData);
+    let f = [];
+    let day = [];
+    let i = 0;
+    let weekday = "";
+    fData.list.forEach(x => {
+        let dateCheck = fData.list[i].dt_txt;
+        if (dateCheck.includes("15:00:00")) {
+            f.push(fData.list[i].main.temp);
+            let d = new Date(dateCheck);
+            day.push(d.getDay());
+        }
+        i++;
+    });
+    for (let i = 0; i <= 4; i++) {
+        switch (day[i]) {
+            case 0:
+                weekday = "Sun";
+                break;
+            case 1:
+                weekday = "Mon";
+                break;
+            case 2:
+                weekday = "Tue";
+                break;
+            case 3:
+                weekday = "Wed";
+                break;
+            case 4:
+                weekday = "Thu";
+                break;
+            case 5:
+                weekday = "Fri";
+                break;
+            case 6:
+                weekday = "Sat";
+                break;
+        }
+
+        document.getElementById("fDay" + i).innerHTML=Math.round(f[i]) + "&deg;F";
+        document.getElementById("wDay" + i).innerHTML=weekday;
+
+    };
+
+
+}
+
---
+++ assignments/weathersite/js/weathersodasprings.js
@@ -0,0 +1,25 @@
+//This file pushes api to the Weather Summary//
+let weatherSource = new XMLHttpRequest();
+let url = "https://api.openweathermap.org/data/2.5/weather?id=5607916&appid=928cdffb6f7b8cf5714b4c2f8047e814&units=imperial";
+
+weatherSource.open("GET", url, true);
+
+weatherSource.send();
+
+weatherSource.onload = function() {
+    let weatherInfo = JSON.parse(weatherSource.responseText);
+    console.log(weatherInfo);
+
+    document.getElementById("rightnow").innerHTML = weatherInfo.weather["0"].description;
+    document.getElementById("precip").innerHTML = weatherInfo.weather["0"].description;
+    document.getElementById("currenttemp").innerHTML = weatherInfo.main.temp;
+    document.getElementById("humid").innerHTML = weatherInfo.main.humidity;
+    document.getElementById("windspeed").innerHTML = weatherInfo.wind.speed;
+
+    let weatherIcon = weatherInfo.weather["0"].icon;
+    let iconLocation = "https://openweathermap.org/img/w/" + weatherIcon + ".png";
+
+    document.getElementById("conditionsnow").src = iconLocation;
+    document.getElementById("now").innerHTML = weatherInfo.weather["0"].main;
+    
+}
